0ade727db2d7dd40f9858d353bbb2fc80b4b49eb
Christian Koch <ckoch@ncsasports.org>
2016-10-09 16:09:37 -0500

    Add test to signature validation

--- lib/ruby/ruby.rb
+++ lib/ruby/ruby.rb
@@ -16,18 +16,11 @@ end
 
 def request(request_method, path, params, content_type, date, content_digest, public_key, private_key, signing_hash = :sha512, options={}, elixir_signature)
   f = StringIO.new(params)
+  digest = Digest::MD5.hexdigest(params)
 
-  # request = OpenStruct.new(request_method: request_method, content_type: content_type,path: path, params: params, date:date, content_digest: content_digest, body: params.to_json, env: {"rack.input" => f, "HTTP_DATE" => date})
-  request = Rack::Request.new(request_method: request_method, content_type: content_type,path: path, params: params, date:date, content_digest: content_digest, body: params.to_json, "rack.input" => f, "HTTP_DATE" => date, "HTTP_AUTHORIZATION" => elixir_signature )
-  # ey = Ey::Hmac::Adapter::Rack.new(request, {sign_with: signing_hash, service: "NCSA.HMAC"})
-  # ey.sign!(public_key, private_key)
-  p "&"*77
-  p request
+  request = Rack::Request.new("REQUEST_METHOD" => request_method, "CONTENT_TYPE" => content_type, "PATH_INFO" => path, "HTTP_CONTENT_DIGEST" => digest, "rack.input" => f, "HTTP_DATE" => date, "HTTP_AUTHORIZATION" => elixir_signature )
 
   Ey::Hmac.authenticated?(request, adapter: adapter) do |auth_id|
-    p auth_id
-    # p public_key
-    # p private_key
     (auth_id == public_key) && private_key
   end
 end
