e416f45f4c102a56b447e909f34c9ccf6c65980f
Christian Koch <ckochx@gmail.com>
2016-10-09 22:54:16 -0500

    Refactor and add tests for other HTTP methods

--- test/ncsa_hmac/cross_validation/ruby_test.exs
+++ test/ncsa_hmac/cross_validation/ruby_test.exs
@@ -13,38 +13,47 @@ defmodule NcsaHmac.CrossValidation.RubyTest do
   @date "Fri, 22 Jul 2016"
   @hash_type :sha256
 
-  test "Validate cryptographic signature against the Ey::Hmac library :sha256" do
-    request_details = %{
-      "path" => @path,
-      "method" => @method,
-      "params" => @params,
-      "date" => @date,
-      "content-type" => @content_type,
-    }
-
-    ey_signature = RubyCall.ruby_call(@method, @path, normalize_parameters, @content_type, @date, @public_key, @private_key, @hash_type)
-
-    ex_signature = Signer.signature(request_details, @private_key, @hash_type)
-    assert ey_signature == ex_signature
+  test "POST, Validate cryptographic signature against the Ey::Hmac library :sha256" do
+    validate_signature("POST", :sha256)
   end
 
-  test "Validate cryptographic signature against the Ey::Hmac library :sha512" do
-    request_details = %{
-      "path" => @path,
-      "method" => @method,
-      "params" => @params,
-      "date" => @date,
-      "content-type" => @content_type,
-    }
-
-    hash_type = :sha512
-    ey_signature = RubyCall.ruby_call(@method, @path, normalize_parameters, @content_type, @date, @public_key, @private_key, hash_type)
-
-    ex_signature = Signer.signature(request_details, @private_key, hash_type)
-    assert ey_signature == ex_signature
+  test "PUT, Validate cryptographic signature against the Ey::Hmac library :sha256" do
+    validate_signature("PUT", :sha256)
   end
 
-  test "Validate cryptographic signature for HTTP_GET against the Ey::Hmac library :sha256" do
+  test "PATCH, Validate cryptographic signature against the Ey::Hmac library :sha256" do
+    validate_signature("PATCH", :sha256)
+  end
+
+  test "DELETE, Validate cryptographic signature against the Ey::Hmac library :sha256" do
+    validate_signature("DELETE", :sha256)
+  end
+
+  test "HEAD, Validate cryptographic signature against the Ey::Hmac library :sha256" do
+    validate_signature("HEAD", :sha256)
+  end
+
+  test "POST, Validate cryptographic signature against the Ey::Hmac library :sha512" do
+    validate_signature("POST", :sha512)
+  end
+
+  test "PUT, Validate cryptographic signature against the Ey::Hmac library :sha512" do
+    validate_signature("PUT", :sha512)
+  end
+
+  test "PATCH, Validate cryptographic signature against the Ey::Hmac library :sha512" do
+    validate_signature("PATCH", :sha512)
+  end
+
+  test "DELETE, Validate cryptographic signature against the Ey::Hmac library :sha512" do
+    validate_signature("DELETE", :sha512)
+  end
+
+  test "HEAD, Validate cryptographic signature against the Ey::Hmac library :sha512" do
+    validate_signature("HEAD", :sha512)
+  end
+
+  test "GET Validate cryptographic signature against the Ey::Hmac library :sha256" do
     query_string = "?abc='123'"
     concat_path = @path <> query_string
     request_details = %{
@@ -55,42 +64,13 @@ defmodule NcsaHmac.CrossValidation.RubyTest do
       "date" => @date,
       "content-type" => @content_type,
     }
-
     ey_signature = RubyCall.ruby_call("get", concat_path, nil, @content_type, @date, @public_key, @private_key, @hash_type)
-
     ex_signature = Signer.signature(request_details, @private_key, @hash_type)
+
     assert ey_signature == ex_signature
   end
 
-  test "Check the Ey::Hmac lib can authenticate an NcsaHmac POST signature" do
-    request_details = %{
-      "path" => @path,
-      "method" => @method,
-      "params" => @params,
-      "date" => @date,
-      "content-type" => @content_type,
-    }
-
-    ex_signature = Signer.sign(request_details, @public_key, @private_key, @hash_type)
-    ey_authenticated = RubyCall.ruby_call_request(@method, @path, normalize_parameters, @content_type, @date, @public_key, @private_key, @hash_type, ex_signature)
-    assert ey_authenticated == true
-  end
-
-  test "Check the Ey::Hmac lib can authenticate an NcsaHmac PUT signature" do
-    request_details = %{
-      "path" => @path,
-      "method" => "put",
-      "params" => @params,
-      "date" => @date,
-      "content-type" => @content_type,
-    }
-
-    ex_signature = Signer.sign(request_details, @public_key, @private_key, @hash_type)
-    ey_authenticated = RubyCall.ruby_call_request("put", @path, normalize_parameters, @content_type, @date, @public_key, @private_key, @hash_type, ex_signature)
-    assert ey_authenticated == true
-  end
-
-  test "Check the Ey::Hmac lib can authenticate an NcsaHmac GET signature" do
+  test "GET Validate cryptographic signature against the Ey::Hmac library :sha512" do
     query_string = "?abc='123'"
     concat_path = @path <> query_string
     request_details = %{
@@ -101,13 +81,75 @@ defmodule NcsaHmac.CrossValidation.RubyTest do
       "date" => @date,
       "content-type" => @content_type,
     }
+    ey_signature = RubyCall.ruby_call("get", concat_path, nil, @content_type, @date, @public_key, @private_key, :sha512)
+    ex_signature = Signer.signature(request_details, @private_key, :sha512)
 
+    assert ey_signature == ex_signature
+  end
+
+  test "Check the Ey::Hmac lib can authenticate an NcsaHmac GET request" do
+    query_string = "?abc='123'"
+    concat_path = @path <> query_string
+    request_details = %{
+      "path" => concat_path,
+      "method" => "get",
+      "params" => "something to get sliced off",
+      "query_string" => query_string,
+      "date" => @date,
+      "content-type" => @content_type,
+    }
     ex_signature = Signer.sign(request_details, @public_key, @private_key, @hash_type)
-    ey_authenticated = RubyCall.ruby_call_request("get", concat_path, "", @content_type, @date, @public_key, @private_key, @hash_type, ex_signature)
+    ey_authenticated = RubyCall.ruby_call_authenticate("get", concat_path, "", @content_type, @date, @public_key, @private_key, @hash_type, ex_signature)
+    
     assert ey_authenticated == true
+  end
+
+  test "Check the Ey::Hmac lib can authenticate an NcsaHmac POST request" do
+    authenticate("POST")
+  end
+
+  test "Check the Ey::Hmac lib can authenticate an NcsaHmac PUT request" do
+    authenticate("PUT")
+  end
+
+  test "Check the Ey::Hmac lib can authenticate an NcsaHmac DELETE request" do
+    authenticate("DELETE")
+  end
+
+  test "Check the Ey::Hmac lib can authenticate an NcsaHmac HEAD request" do
+    authenticate("HEAD")
+  end
+
+  test "Check the Ey::Hmac lib can authenticate an NcsaHmac PATCH request" do
+    authenticate("patch")
+  end
+
+  def validate_signature(method, hash_type) do
+    request_details = request_details(method)
+    ey_signature = RubyCall.ruby_call(method, @path, normalize_parameters, @content_type, @date, @public_key, @private_key, hash_type)
+    ex_signature = Signer.signature(request_details, @private_key, hash_type)
+
+    assert ey_signature == ex_signature
+  end
+
+  def authenticate(method) do
+    request_details = request_details(method)
+    ex_signature = Signer.sign(request_details, @public_key, @private_key, @hash_type)
+    ey_authenticated = RubyCall.ruby_call_authenticate(method, @path, normalize_parameters, @content_type, @date, @public_key, @private_key, @hash_type, ex_signature)
+    
+    assert ey_authenticated == true    
   end
 
   defp normalize_parameters do
     Signer.normalize_parameters @params
+  end
+
+  defp request_details(method) do
+    %{"path" => @path,
+      "method" => method,
+      "params" => @params,
+      "date" => @date,
+      "content-type" => @content_type
+    }
   end
 end
