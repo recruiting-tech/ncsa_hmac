a3e19203007f91b78857deff8689e487fd2959a7
Christian Koch <ckoch@ncsasports.org>
2018-10-01 10:50:32 -0500

    relax json restriction version

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule NcsaHmac.Mixfile do
     [
       {:ecto, ">= 1.1.0"},
       {:timex, "~> 3.0"},
-      {:json, "~> 0.3.0"},
+      {:json, ">= 1.0.0"},
       {:plug, "~> 1.0"},
       {:earmark, ">= 0.0.0", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev},
